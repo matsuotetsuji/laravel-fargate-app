dd5a052d21b2cd7b0e24f646ec274d1ca731ee81
matsuotetsuji <103009758+matsuotetsuji@users.noreply.github.com>
2023-03-29 17:55:58 +0900

    user has multi post. php artisan migrate:fresh --seed

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,6 +23,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Post::factory(30)->create();
+        //Post::factory(30)->create();
+        User::factory(15)->create()->each(function ($user) {
+            Post::factory(random_int(2, 5))->create(['user_id' => $user]);
+        });
     }
 }
