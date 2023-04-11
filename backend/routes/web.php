4deefc9213660cfc2f76b77fe92ff610c793da2b
matsuotetsuji <103009758+matsuotetsuji@users.noreply.github.com>
2023-04-11 17:31:18 +0900

    Valid login params.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -26,6 +26,7 @@ Route::get('signup', [SignupController::class, 'index']);
 Route::post('signup', [SignupController::class, 'store']);
 
 Route::get('mypage/login', [UserLoginController::class, 'index']);
+Route::post('mypage/login', [UserLoginController::class, 'login']);
 
 /**ß
 Route::get('/', function () {
