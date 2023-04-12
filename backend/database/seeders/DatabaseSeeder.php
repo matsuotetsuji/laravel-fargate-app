40b7e1b6fa856f2c9cdfb662289bfcd670490008
matsuotetsuji <103009758+matsuotetsuji@users.noreply.github.com>
2023-04-12 15:57:26 +0900

    Confirm the site on Browser.

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,10 +26,16 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         //Post::factory(30)->create();
-        User::factory(15)->create()->each(function ($user) {
+        [$first] = User::factory(15)->create()->each(function ($user) {
             Post::factory(random_int(2, 5))->random()->create(['user_id' => $user])->each(function ($post) {
                 Comment::factory(random_int(1, 5))->create(['post_id' => $post]);
             });
         });
+
+        $first->update([
+            'name' => '松尾',
+            'email' => 'aaa@bbb.net',
+            'password' => Hash::make('hogehoge'),
+        ]);
     }
 }
