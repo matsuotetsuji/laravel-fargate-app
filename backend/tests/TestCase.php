e3ceed20409fb0ec657ade7c5728f6161f2fc5a9
matsuotetsuji <103009758+matsuotetsuji@users.noreply.github.com>
2023-04-12 11:47:33 +0900

    Valid auth to open mypage

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -10,4 +11,13 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
     use RefreshDatabase;
+
+    public function login(User $user = null)
+    {
+        $user ??= User::factory()->create();
+
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
